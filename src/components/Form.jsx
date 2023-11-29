152809d81a28c5edef51c0c994f202d0b6bd90d9
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2023-11-29 22:28:49 +0500

    feature: handle the submit request, add new user to array

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+import usersData from '../assets/users_data';
+
 const Form = () => {
+  const navigate = useNavigate();
+
+  const addUser = async (e) => {
+    e.preventDefault();
+
+    // Access form data from the event object
+    const { email, name, role } = e.target.form.elements;
+
+    const newUser = {
+      id: usersData.length + 1,
+      email: email.value,
+      name: name.value,
+      role: role.value,
+    };
+
+    // Update the array
+    usersData.push(newUser);
+    console.log(usersData);
+
+    // Navigate to root path
+    navigate('/');
+  };
+
+
   return (
     <>
       <h1>User signup</h1>
@@ -17,14 +44,14 @@ const Form = () => {
 
           <label htmlFor="role">Role</label> <br />
           <select name="role" id="role">
-            <option value="1" selected>User</option>
-            <option value="2">Admin</option>
-            <option value="3">Mod</option>
+            <option value="user" selected>User</option>
+            <option value="admin">Admin</option>
+            <option value="mod">Mod</option>
           </select>
           <br />
           <br />
 
-          <button type="submit">Submit</button>
+          <button onClick={addUser} type="submit">Submit</button>
         </form>
       </div>
     </>
