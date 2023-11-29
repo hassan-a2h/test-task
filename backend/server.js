3357801c5b952e48896f9b2f54bbc404881fed12
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2023-11-29 20:30:37 +0500

    feature: implement logic to receive the user data
    logic:
    * validate the email field using regex

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const isValidEmail = require('./utilities/email_validator');
+
 const app = express();
 const PORT = 3000;
 
@@ -8,12 +10,29 @@ const users = [
   // Add more sample users as needed
 ];
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Get the server going');
 });
 
-app.get('/api/users', (req, res) => {
+app.get('/users', (req, res) => {
   res.json(users);
+});
+
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  // Simple server-side validation for the email field
+  if (!newUser || !newUser.email || !isValidEmail(newUser.email) || !newUser.name) {
+    return res.status(400).json({ error: 'Invalid user data' });
+  }
+
+  newUser.id = users.length + 1;
+  users.push(newUser);
+
+  console.log('New user added:', newUser);
+  res.json(newUser);
 });
 
 app.listen(PORT, () => {
